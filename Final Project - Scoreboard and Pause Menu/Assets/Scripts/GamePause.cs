832c40ac5650580a3dfe870436b2008887587292
Hannah B <23014673@sfcollege.edu>
2024-10-28 04:49:30 -0400

    Uploading "Final Project - Scoreboard and Pause Menu"

---
+++ Final Project - Scoreboard and Pause Menu/Assets/Scripts/GamePause.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GamePause : MonoBehaviour
+{
+    public TextMeshProUGUI pauseButtonText;
+    public Button pauseButton;
+
+    void Start()
+    {
+        if (pauseButton != null)
+        {
+            pauseButton.onClick.AddListener(TogglePause);
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (!GameManager.isGameActive) return;
+
+        if (GameManager.isPaused)
+        {
+            Time.timeScale = 1f;
+
+            pauseButtonText.text = "Pause";
+
+            GameManager.isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+
+            pauseButtonText.text = "Resume";
+
+            GameManager.isPaused = true;
+        }
+    }
+}
